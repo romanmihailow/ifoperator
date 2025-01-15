5d6367e6cb1b06aa34bf85fe9f0cf9ad258a9978
romanmihailow <romanmihailow@gmail.com>
2025-01-15 11:43:55 +0500

    change3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        int x = 5;
+        int x = -5;
+        if (x > 0) {
+            System.out.println("X - Положительный");
+        } else {
+            System.out.println("X - Отрицательный");
+        }
 
-        System.out.println(x);
+        System.out.println("Bay");
     }
 
 
